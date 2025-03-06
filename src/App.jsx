78ffb2db1a27d56cef13d934ad0aff81923e951b
Hemant Dhote <hemantdhote76@gmail.com>
2025-03-06 16:40:30 +0530

    components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  const data=["service","about","contact"]
+  return (
+    <div>
+
+      <Navbar />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  
+  return (
+    <div>
+
+<div className='nav bg-neutral-100 w-full p-5 flex items-center justify-between'>
+        <div className='left text-xl font-bold'>LOGO</div>
+        <div className='right flex items-center gap-8 text-lg'>
+
+          <h4>About </h4>
+          <h4>services</h4>
+          <h4>client</h4>
+          <h4>contact</h4>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
